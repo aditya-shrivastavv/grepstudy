fbbfa701293bf0b1cddb3da35fd0390fe54bec59
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-08-19 09:33:59 +0530

    created controllers for sendOtp and SignUp

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,6 +1,167 @@
-// send otp
+const User = require('../models/User')
+const OTP = require('../models/OTP')
+const otpGenerator = require('otp-generator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-// signup
+//sendOTP
+exports.sendOTP = async (req, res) => {
+  try {
+    //fetch email from request body
+    const { email } = req.body
+
+    const checkUserPresent = await User.findOne({ email })
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: 'User already registered',
+      })
+    }
+
+    //generate otp
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    })
+    console.log('OTP generated: ', otp)
+
+    //check unique otp or not
+    let result = await OTP.findOne({ otp: otp })
+    while (result) {
+      otp = otpGenerator(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      })
+      result = await OTP.findOne({ otp: otp })
+    }
+
+    //create an entry for OTP
+    const otpPayload = { email, otp }
+    const otpBody = await OTP.create(otpPayload)
+    console.log(otpBody)
+
+    //return response successful
+    res.status(200).json({
+      success: true,
+      message: 'OTP Sent Successfully',
+      otp,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
+
+//signUp
+exports.signUp = async (req, res) => {
+  try {
+    //data fetch from request ki body
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      accountType,
+      contactNumber,
+      otp,
+    } = req.body
+
+    //validate krlo
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !otp
+    ) {
+      return res.status(403).json({
+        success: false,
+        message: 'All fields are required',
+      })
+    }
+
+    //2 password match krlo
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message:
+          'Password and ConfirmPassword Value does not match, please try again',
+      })
+    }
+
+    //check user already exist or not
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: 'User is already registered',
+      })
+    }
+
+    //find most recent OTP stored for the user
+    const recentOtp = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1)
+    console.log(recentOtp)
+
+    //validate OTP
+    if (recentOtp.length == 0) {
+      //OTP not found
+      return res.status(400).json({
+        success: false,
+        message: 'OTPP Found',
+      })
+    } else if (otp !== recentOtp.otp) {
+      //Invalid OTP
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid OTP',
+      })
+    }
+
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    //entry create in DB
+
+    const profileDetails = await Profile.create({
+      gender: null,
+      dateOfBirth: null,
+      about: null,
+      contactNumer: null,
+    })
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      contactNumber,
+      password: hashedPassword,
+      accountType,
+      additionalDetails: profileDetails._id,
+      image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstname} ${lastName}`,
+    })
+
+    //return res
+    return res.status(200).json({
+      success: true,
+      message: 'User is registered Successfully',
+      user,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      success: false,
+      message: 'User cannot be registrered. Please try again',
+    })
+  }
+}
 
 // login
 
