2d2d62d3d3f37bd267b7bf1f11d0cfdd883c5638
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-29 17:00:05 +0530

    added vdo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,6 +44,17 @@ function Home() {
           Book a Demo
         </ButtonsYB>
       </div>
+
+      <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
+        <video
+          className="shadow-[20px_20px_rgba(255,255,255)]"
+          muted
+          loop
+          autoPlay
+        >
+          <source src={Banner} type="video/mp4" />
+        </video>
+      </div>
     </div>
   )
 }
