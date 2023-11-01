9a2ddf91169660509264f84fa56f319b49d6d2db
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-11-01 09:23:44 +0530

    add docs

--- server/utils/uploadToCloudinary.js
+++ server/utils/uploadToCloudinary.js
@@ -1,5 +1,13 @@
 const cloudinary = require('cloudinary').v2
 
+/**
+ * Uploads a file to Cloudinary.
+ * @param {Object} file - The file to upload.
+ * @param {string} folder - The folder in which to store the file.
+ * @param {number} [height] - The height to resize the image to.
+ * @param {number} [quality] - The quality of the image.
+ * @returns {Promise<Object>} - A Promise that resolves to the uploaded file object.
+ */
 exports.uploadToCloudinary = async (file, folder, height, quality) => {
   const options = { folder }
   if (height) options.height = height
